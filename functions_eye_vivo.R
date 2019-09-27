f201d6833cf80e7abad17845c3457e9e12c30f55
Chris <cjbouwens@gmail.com>
2019-09-27 01:16:20 +0100

    Update functions_eye_vivo.R

--- functions_eye_vivo.R
+++ functions_eye_vivo.R
@@ -3,7 +3,8 @@
 # Calulate neccesary and interesting parameters of in-vivo experiments.
 
 # dataprep
-dataprep <- function(file, outputloc) {
+dataprep <- function(file) {
+  # returns a matrix of the volumetric data, with dates as column names.
   if (!require(data.table)) install.packages('data.table')
   suppressPackageStartupMessages(library(data.table))
   
@@ -11,9 +12,10 @@ dataprep <- function(file, outputloc) {
   micematrix <- as.matrix(micedata[,-c(1:4)])
   col <- colnames(micematrix)
   colnames(micematrix) <- as.character(as.Date(col, format ="%d/%m/%Y"))
+  return(micematrix)
 }
 
-startpick <- function(data = micematrix , weight = 90){ 
+startpick <- function(data = micemat , weight = 90){ 
   # returns experimental starting date from a matrix of the format:
   # rows = mice, columns = dates, fill = tumour volume.
   #
@@ -22,26 +24,26 @@ startpick <- function(data = micematrix , weight = 90){
   # date as column.
   #       weight = the parameter from which experiment start should be counted.
   startdate <- c()
-  for (row in seq_along(1:nrow(micematrix))) {
-  startdate[row] <- colnames(micematrix)[which(micematrix[row, ] >= 90,
+  dat <- data
+  for (row in seq_along(1:nrow(dat))) {
+  startdate[row] <- colnames(dat)[which(dat[row, ] >= 90,
                                           arr.ind = TRUE)[1]]
   startdate <- as.Date(startdate, format = "%d/%m/%Y")
   return(startdate)
   }
 }
 
-exprun <- function(start = startdate, curdate = Sys.Date()) {
+exprun <- function(start = begin, curdate = Sys.Date()) {
   # how many days has each mice been in treatment?
   # 
   # Args: startdate = the date from when the mice started treatment.
   #       curdate = the current date, or the date to which experiment end.
   # (TODO) : what if an animal has been culled before the current date?
-  print(Sys.Date())
   treatment_time <- curdate - start
   return(as.vector(treatment_time))
 }
 
-growth <- function(matrix, start) {
+growth <- function(matrix = micematrix, startdate = begin) {
   # grabs the earliest measured tumour size above 90 mm and 
   # substracts the last measured tumour size.
   #
@@ -51,13 +53,13 @@ growth <- function(matrix, start) {
   # (note these should be R Date format, not characters)
   # 
   # Returns: a vector with size differences per mice.
-  miceDT <- as.data.table(micematrix)
+  miceDT <- as.data.table(matrix)
   startweight <- c()
   endweight <- c()
-  
   #grab the tumour size on the start day
-  for (n in seq_along(1 : length(startdate))) {
-    startweight[n] <- miceDT[n, as.character(startdate[n]), with = F]
+  startchar <- as.character(startdate)
+  for (n in seq_along(1 : length(startchar))) {
+    startweight[n] <- miceDT[n, get(startchar)[n], with = F]
   }
   startweight <- unlist(startweight, use.names = F)
   
@@ -147,6 +149,17 @@ growthwum <- apply(intermatrix, MARGIN = 1, function(x){
 # out: dataframe (list per animal/group):
 #     interval, growth or remiss, how much?, total growth and remis intervals
 
+setwd("~/Documents/car")
+micemat <- dataprep("~/Downloads/mice_volume.csv")
+col <- colnames(micemat)
+first <- startpick(data = micemat, weight = 90)  #invalid first argument. am i in the wrong calling scope?
+treatmenttime <- exprun(start = first, curdate = Sys.Date())
+tumourgrowth <- growth(matrix = micemat, startdate = first)
+  
+  results <- cbind(startdate, treatmenttime, tumourgrowth)
+  return(results)
+  
 
+mainvivo(data = "~/Downloads/mice_volume.csv", weight = 90, output = "~/Documents/car")
 # (TODO) create growth graphs
 # tumrouprog <- function(){}
