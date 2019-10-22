ef4395ef9596445a22f88e1ef7896e90fd99b9f7
Chris <cjbouwens@gmail.com>
2019-10-22 17:04:54 +0100

    added vignette and removed param description bugs

---
+++ doc/rvivointro.R
@@ -0,0 +1,20 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, eval = FALSE-------------------------------------------------
+#  library(Rvivo)
+#  # to get parameter explanation type ?rvivo
+#  rvivo(volumes = NULL,
+#        measures = "path/to/measures.csv",
+#        cul = "/path/to/cull.csv",
+#        minvol = 90,
+#        colstyle = "Dark2",
+#        figures = TRUE,
+#        table = TRUE,
+#        axis = "linear",
+#        survival = TRUE,
+#        output = "/path/to/outputfolder")
+
